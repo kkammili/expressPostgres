fbb84504180e1b16bd8869e0c062493c740e0829
kkammili <kkrajus777@gmail.com>
2025-07-30 17:20:59 -0400

    created a sample docker file

--- db/index.js
+++ db/index.js
@@ -1,13 +1,18 @@
-const Pool = require('pg').Pool;
-const secrets = require('../secrets/db_configuration')
+const Pool = require("pg").Pool;
+// const secrets = require("../secrets/db_configuration");
 
-// const secrets = {
-//     user: 'node_user',
-//     password:'node_password',
-//     host:'localhost',
-//     database:'monstersdb',
-//     port:5432
-// }
+const secrets = {
+  user: "monsteruser",
+  password: "monsterpass",
+  host: "db",
+  database: "monsterdb",
+  port: 5432,
+};
+
+//    DB_USER: monsteruser
+//       DB_HOST: db
+//       DB_DATABASE: monsterdb
+//       DB_PASSWORD: monsterpass
 
 const pool = new Pool(secrets);
-module.exports = pool
+module.exports = pool;
